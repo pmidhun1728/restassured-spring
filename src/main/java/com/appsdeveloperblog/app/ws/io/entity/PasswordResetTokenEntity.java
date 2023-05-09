70d4374b12f47a0a36f9ce07767ab72533d806d0
Midhun <42476012+pmidhun1728@users.noreply.github.com>
2023-05-09 15:15:18 -0400

    Framework Spring code

---
+++ src/main/java/com/appsdeveloperblog/app/ws/io/entity/PasswordResetTokenEntity.java
@@ -0,0 +1,52 @@
+package com.appsdeveloperblog.app.ws.io.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import java.io.Serializable;
+ 
+@Entity(name = "password_reset_tokens")
+public class PasswordResetTokenEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8051324316462829780L;
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	private String token;
+
+	@OneToOne()
+	@JoinColumn(name = "users_id")
+	private UserEntity userDetails;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public UserEntity getUserDetails() {
+		return userDetails;
+	}
+
+	public void setUserDetails(UserEntity userDetails) {
+		this.userDetails = userDetails;
+	}
+
+}
