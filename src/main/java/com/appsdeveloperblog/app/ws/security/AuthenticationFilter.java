d88d3d1c922779d481722d9f45fe134ea3bab2c8
Midhun <42476012+pmidhun1728@users.noreply.github.com>
2023-05-09 15:35:44 -0400

    Code changes for Authentication Filters

--- src/main/java/com/appsdeveloperblog/app/ws/security/AuthenticationFilter.java
+++ src/main/java/com/appsdeveloperblog/app/ws/security/AuthenticationFilter.java
@@ -1,13 +1,17 @@
 package com.appsdeveloperblog.app.ws.security;
 
 import java.io.IOException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Date;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.appsdeveloperblog.app.ws.SpringApplicationContext;
@@ -23,52 +27,60 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final AuthenticationManager authenticationManager;
 
     public AuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
     }
-    
+
     @Override
     public Authentication attemptAuthentication(HttpServletRequest req,
                                                 HttpServletResponse res) throws AuthenticationException {
         try {
- 
-        	UserLoginRequestModel creds = new ObjectMapper()
+
+            UserLoginRequestModel creds = new ObjectMapper()
                     .readValue(req.getInputStream(), UserLoginRequestModel.class);
-            
+
             return authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
                             creds.getEmail(),
                             creds.getPassword(),
                             new ArrayList<>())
             );
-            
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    
+
     @Override
     protected void successfulAuthentication(HttpServletRequest req,
                                             HttpServletResponse res,
                                             FilterChain chain,
                                             Authentication auth) throws IOException, ServletException {
-        
-        String userName = ((UserPrincipal) auth.getPrincipal()).getUsername();  
-        
+
+
+        byte[] secretKeys = Base64.getDecoder().decode(SecurityConstants.getTokenSecret());
+        Key key = new SecretKeySpec(secretKeys,"HmacSHA512");
+        String userName = ((User) auth.getPrincipal()).getUsername();
+
         String token = Jwts.builder()
                 .setSubject(userName)
                 .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS512, SecurityConstants.getTokenSecret() )
+                .signWith(key,SignatureAlgorithm.HS512 )
                 .compact();
         UserService userService = (UserService)SpringApplicationContext.getBean("userServiceImpl");
         UserDto userDto = userService.getUser(userName);
-        
+
         res.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
         res.addHeader("UserID", userDto.getUserId());
 
-    }  
+
+    }
 
 }
--- src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -44,16 +44,16 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	BCryptPasswordEncoder bCryptPasswordEncoder;
-	
-	@Autowired 
-	PasswordResetTokenRepository passwordResetTokenRepository;
-	
+
 	@Autowired
-    AmazonSES amazonSES;
-	
+	PasswordResetTokenRepository passwordResetTokenRepository;
+
+	@Autowired
+	AmazonSES amazonSES;
+
 	@Autowired
 	RoleRepository roleRepository;
- 
+
 	@Override
 	public UserDto createUser(UserDto user) {
 
@@ -67,7 +67,7 @@ public class UserServiceImpl implements UserService {
 			address.setAddressId(utils.generateAddressId(30));
 			user.getAddresses().set(i, address);
 		}
-		  
+
 		//BeanUtils.copyProperties(user, userEntity);
 		ModelMapper modelMapper = new ModelMapper();
 		UserEntity userEntity = modelMapper.map(user, UserEntity.class);
@@ -75,9 +75,9 @@ public class UserServiceImpl implements UserService {
 		String publicUserId = utils.generateUserId(30);
 		userEntity.setUserId(publicUserId);
 		userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-		userEntity.setEmailVerificationToken(utils.generateEmailVerificationToken(publicUserId));
-		
-		// Set roles 
+		//userEntity.setEmailVerificationToken(utils.generateEmailVerificationToken(publicUserId));
+
+		// Set roles
 		Collection<RoleEntity> roleEntities = new HashSet<>();
 		for(String role: user.getRoles()) {
 			RoleEntity roleEntity = roleRepository.findByName(role);
@@ -85,15 +85,15 @@ public class UserServiceImpl implements UserService {
 				roleEntities.add(roleEntity);
 			}
 		}
-		
+
 		userEntity.setRoles(roleEntities);
 
 		UserEntity storedUserDetails = userRepository.save(userEntity);
- 
+
 		//BeanUtils.copyProperties(storedUserDetails, returnValue);
 		UserDto returnValue  = modelMapper.map(storedUserDetails, UserDto.class);
-		
-        // Send an email message to user to verify their email address
+
+		// Send an email message to user to verify their email address
 		//amazonSES.verifyEmail(returnValue);
 
 		return returnValue;
@@ -108,7 +108,7 @@ public class UserServiceImpl implements UserService {
 
 		UserDto returnValue = new UserDto();
 		BeanUtils.copyProperties(userEntity, returnValue);
- 
+
 		return returnValue;
 	}
 
@@ -118,10 +118,10 @@ public class UserServiceImpl implements UserService {
 
 		if (userEntity == null)
 			throw new UsernameNotFoundException(email);
-		
-		return new UserPrincipal(userEntity);
 
-		// return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), 
+		return new User(email,userEntity.getEncryptedPassword(),new ArrayList<>());
+
+		// return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(),
 		// 		userEntity.getEmailVerificationStatus(),
 		// 		true, true,
 		// 		true, new ArrayList<>());
@@ -175,101 +175,101 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public List<UserDto> getUsers(int page, int limit) {
 		List<UserDto> returnValue = new ArrayList<>();
-		
+
 		if(page>0) page = page-1;
-		
+
 		Pageable pageableRequest = PageRequest.of(page, limit);
-		
+
 		Page<UserEntity> usersPage = userRepository.findAll(pageableRequest);
 		List<UserEntity> users = usersPage.getContent();
-		
-        for (UserEntity userEntity : users) {
-            UserDto userDto = new UserDto();
-            BeanUtils.copyProperties(userEntity, userDto);
-            returnValue.add(userDto);
-        }
-		
+
+		for (UserEntity userEntity : users) {
+			UserDto userDto = new UserDto();
+			BeanUtils.copyProperties(userEntity, userDto);
+			returnValue.add(userDto);
+		}
+
 		return returnValue;
 	}
 
 	@Override
 	public boolean verifyEmailToken(String token) {
-	    boolean returnValue = false;
+		boolean returnValue = false;
 
-        // Find user by token
-        UserEntity userEntity = userRepository.findUserByEmailVerificationToken(token);
+		// Find user by token
+		UserEntity userEntity = userRepository.findUserByEmailVerificationToken(token);
 
-        if (userEntity != null) {
-            boolean hastokenExpired = Utils.hasTokenExpired(token);
-            if (!hastokenExpired) {
-                userEntity.setEmailVerificationToken(null);
-                userEntity.setEmailVerificationStatus(Boolean.TRUE);
-                userRepository.save(userEntity);
-                returnValue = true;
-            }
-        }
+		if (userEntity != null) {
+			boolean hastokenExpired = Utils.hasTokenExpired(token);
+			if (!hastokenExpired) {
+				userEntity.setEmailVerificationToken(null);
+				userEntity.setEmailVerificationStatus(Boolean.TRUE);
+				userRepository.save(userEntity);
+				returnValue = true;
+			}
+		}
 
-        return returnValue;
+		return returnValue;
 	}
 
 	@Override
 	public boolean requestPasswordReset(String email) {
-		
-        boolean returnValue = false;
-        
-        UserEntity userEntity = userRepository.findByEmail(email);
 
-        if (userEntity == null) {
-            return returnValue;
-        }
-        
-        String token = new Utils().generatePasswordResetToken(userEntity.getUserId());
-        
-        PasswordResetTokenEntity passwordResetTokenEntity = new PasswordResetTokenEntity();
-        passwordResetTokenEntity.setToken(token);
-        passwordResetTokenEntity.setUserDetails(userEntity);
-        passwordResetTokenRepository.save(passwordResetTokenEntity);
-        
-        returnValue = new AmazonSES().sendPasswordResetRequest(
-                userEntity.getFirstName(), 
-                userEntity.getEmail(),
-                token);
-        
+		boolean returnValue = false;
+
+		UserEntity userEntity = userRepository.findByEmail(email);
+
+		if (userEntity == null) {
+			return returnValue;
+		}
+
+		String token = new Utils().generatePasswordResetToken(userEntity.getUserId());
+
+		PasswordResetTokenEntity passwordResetTokenEntity = new PasswordResetTokenEntity();
+		passwordResetTokenEntity.setToken(token);
+		passwordResetTokenEntity.setUserDetails(userEntity);
+		passwordResetTokenRepository.save(passwordResetTokenEntity);
+
+		returnValue = new AmazonSES().sendPasswordResetRequest(
+				userEntity.getFirstName(),
+				userEntity.getEmail(),
+				token);
+
 		return returnValue;
 	}
 
 	@Override
 	public boolean resetPassword(String token, String password) {
-        boolean returnValue = false;
-        
-        if( Utils.hasTokenExpired(token) )
-        {
-            return returnValue;
-        }
- 
-        PasswordResetTokenEntity passwordResetTokenEntity = passwordResetTokenRepository.findByToken(token);
+		boolean returnValue = false;
 
-        if (passwordResetTokenEntity == null) {
-            return returnValue;
-        }
+		if( Utils.hasTokenExpired(token) )
+		{
+			return returnValue;
+		}
 
-        // Prepare new password
-        String encodedPassword = bCryptPasswordEncoder.encode(password);
-        
-        // Update User password in database
-        UserEntity userEntity = passwordResetTokenEntity.getUserDetails();
-        userEntity.setEncryptedPassword(encodedPassword);
-        UserEntity savedUserEntity = userRepository.save(userEntity);
- 
-        // Verify if password was saved successfully
-        if (savedUserEntity != null && savedUserEntity.getEncryptedPassword().equalsIgnoreCase(encodedPassword)) {
-            returnValue = true;
-        }
-   
-        // Remove Password Reset token from database
-        passwordResetTokenRepository.delete(passwordResetTokenEntity);
-        
-        return returnValue;
+		PasswordResetTokenEntity passwordResetTokenEntity = passwordResetTokenRepository.findByToken(token);
+
+		if (passwordResetTokenEntity == null) {
+			return returnValue;
+		}
+
+		// Prepare new password
+		String encodedPassword = bCryptPasswordEncoder.encode(password);
+
+		// Update User password in database
+		UserEntity userEntity = passwordResetTokenEntity.getUserDetails();
+		userEntity.setEncryptedPassword(encodedPassword);
+		UserEntity savedUserEntity = userRepository.save(userEntity);
+
+		// Verify if password was saved successfully
+		if (savedUserEntity != null && savedUserEntity.getEncryptedPassword().equalsIgnoreCase(encodedPassword)) {
+			returnValue = true;
+		}
+
+		// Remove Password Reset token from database
+		passwordResetTokenRepository.delete(passwordResetTokenEntity);
+
+		return returnValue;
 	}
-	
+
 }
